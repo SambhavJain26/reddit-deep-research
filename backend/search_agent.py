61b643af25a96dcd263fa94defa7ff775db59ea1
SambhavJain26 <worksambhav26@gmail.com>
2025-06-17 09:59:21 +0530

    changed prompts

--- backend/search_agent.py
+++ backend/search_agent.py
@@ -2,11 +2,13 @@
 from agents import Agent, WebSearchTool, ModelSettings
 
 INSTRUCTIONS = (
-    "You are a research assistant. Given a search term, you search for reddit posts on the web for that search term and "
+    "You are a reddit research assistant. Given a search term, you search only on reddit for a given search term and "
+    'The way to search on reddit is by putting "site:reddit.com" in the search term.'
     "produce a concise summary of the results. The summary must 2-3 paragraphs and less than 300 "
     "words. Capture the main points. Write succintly, no need to have complete sentences or good "
     "grammar. This will be consumed by someone synthesizing a report, so its vital you capture the "
     "essence and ignore any fluff. Do not include any additional commentary other than the summary itself."
+    "Add the reddit post url to the summary."
 )
 
 search_agent = Agent(
--- backend/writer_agent.py
+++ backend/writer_agent.py
@@ -5,11 +5,11 @@ from agents import Agent
 INSTRUCTIONS = (
     "You are a senior researcher tasked with writing a cohesive report for a research query. "
     "You will be provided with the original query, and some initial research done by a research assistant.\n"
-    "You should first come up with an outline for the report that describes the structure and "
-    "flow of the report.The topics of the report should be highlighted based on the query."
-    "Then, generate the report and return that as your final output.\n"
-    "The final output should be in markdown format, and it should cover the query in detail. "
-    "Not too long, but not too short. All the information and topics should be covered in detail."
+    "You should first come up with an outline for the report based on the query that describes the structure and "
+    "flow of the report. The topics of the report should be highlighted based on the research done by the research assistant."
+    "Then, generate the report and return that as your final output. Add the reddit post url to the report in the references section.\n"
+    "The final output should be in markdown format, and it should cover the research done by the research assistant briefly "
+    "Maximum 500 words. The report should be concise and to the point."
 )
 
 
